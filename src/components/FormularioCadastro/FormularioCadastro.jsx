4630f04c9d3e683f06ee31a04a08207f2910f6ba
João Victor <joaovictordjardim@gmail.com>
2021-01-28 17:55:54 -0300

    Melhorando o design da pagina

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -4,10 +4,20 @@ import './style.css';
 class FormularioCadastro extends Component {
 	render() {
 		return (
-			<form>
-				<input type="text" placeholder ="Título"/>
-				<textarea placeholder = "Escreva sua nota..."/>
-				<button>Criar nota</button>
+			<form className="form-cadastro">
+				<input 
+					type="text" 
+					placeholder="Título"
+					className="form-cadastro_input"
+				/>
+				<textarea 
+					rows={15}
+					placeholder="Escreva sua nota..."
+					className="form-cadastro_input"
+				/>
+				<button className="form-cadastro_input form-cadastro_submit">
+					Criar nota
+				</button>
 			</form>
 		)
 	}
