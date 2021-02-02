598b2fee0e4c798ba03ebb5e5fd081506f46db0c
joaovictordjardim <joaovictordjardim@gmail.com>
2021-02-02 09:55:17 -0300

    Projeto Finalizado

--- src/App.js
+++ src/App.js
@@ -1,59 +1,35 @@
-import React, { Component } from "react";
-import ListaDeNotas from "./components/ListaDeNotas";
-import ListaDeCategorias from "./components/ListaDeCategorias";
-import FormularioCadastro from "./components/FormularioCadastro";
-import "./assets/App.css";
+import React, { Component } from 'react';
+import ListaDeNotas from './components/ListaDeNotas';
+import FormularioCadastro from './components/FormularioCadastro';
+import ListaDeCategorias from './components/ListaDeCategorias';
+import './assets/App.css';
 import './assets/index.css';
+import Categorias from './dados/Categorias';
+import ArrayDeNotas from './dados/Notas';
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
-
-    this.state = {
-      notas:[],
-      categorias:['games', 'musica'],
-    }
-  }
-
-  criarNota(titulo, texto, categoria){
-    const novaNota = {titulo, texto, categoria};
-    const novoArrayNotas = [...this.state.notas,novaNota]
-    const novoEstado = {
-      notas:novoArrayNotas
-    }
-    this.setState(novoEstado)
-  }
-
-  adicionarCategoria(nomeCategoria) {
-    const novoArrayCategorias = [...this.state.categorias, nomeCategoria];
-    const novoEstado = {...this.state, categorias:novoArrayCategorias}
-    this.setState(novoEstado)
-  }
-
-  deletarNota(index) {
-    let arrayNotas = this.state.notas;
-    arrayNotas.splice(index,1);
-    console.log('deletar');
-    this.setState({notas:arrayNotas})
+    this.categorias = new Categorias();
+    this.notas = new ArrayDeNotas();
+    
   }
 
   render() {
     return (
-      <section className="conteudo">
+      <section className='conteudo'>
         <FormularioCadastro
-          categorias={this.state.categorias}
-          criarNota={this.criarNota.bind(this)}
+          categorias={this.categorias}
+          criarNota={this.notas.adicionarNota.bind(this.notas)}
         />
-        <main className="conteudo-principal">
+        <main className='conteudo-principal'>
           <ListaDeCategorias
-            categorias={this.state.categorias}
-            adicionarCategoria={this.adicionarCategoria.bind(this)}
+            adicionarCategoria={this.categorias.adicionarCategoria.bind(this.categorias)}
+            categorias={this.categorias}
           />
           <ListaDeNotas
-            apagarNota={this.deletarNota.bind(this)}
-            notas={this.state.notas}
+            apagarNota={this.notas.apagarNota.bind(this.notas)}
+            notas={this.notas}
           />
-
         </main>
       </section>
     );
--- src/components/ListaDeNotas/ListaDeNotas.jsx
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -1,27 +1,44 @@
 import React, { Component } from 'react';
 import CardNota from '../CardNota';
 import './style.css';
-
 class ListaDeNotas extends Component {
-  render () {
+ 
+  constructor(){
+    super();
+    this.state = {notas:[]}
+    this._novasNotas = this._novasNotas.bind(this);
+  }
+  componentDidMount(){
+    this.props.notas.inscrever(this._novasNotas);
+  }
+  componentWillUnmount(){
+    this.props.notas.desinscrever(this._novasNotas);
+  }
+  _novasNotas(notas){
+    this.setState({...this.state,notas})
+  }
+
+  render() {
+
     return (
-      <ul className="lista-notas">
-        {this.props.notas.map((nota, index) => {
+      <ul className='lista-notas'>
+        {this.state.notas.map((nota, index) => {
           return (
-            <li className="lista-notas_item" key={index}>
-              <CardNota
-                indice={index}
-                apagarNota={this.props.apagarNota}
-                titulo={nota.titulo} 
-                texto={nota.texto}
-                categoria={nota.categoria}
+            <li className='lista-notas_item' key={index}>
+              
+              <CardNota 
+              indice={index}
+              apagarNota={this.props.apagarNota}
+              titulo={nota.titulo} 
+              texto={nota.texto}
+              categoria={nota.categoria}
               />
             </li>
           );
         })}
       </ul>
-    )
-  };
+    );
+  }
 }
 
 export default ListaDeNotas;
